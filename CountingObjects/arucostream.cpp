66de9b305affd61813d7676d32c8ee30cc02763b
Timmmvp <tim.van.putten.tvp@gmail.com>
2016-05-27 13:08:20 +0200

    added stream sample module

---
+++ CountingObjects/arucostream.cpp
@@ -0,0 +1,21 @@
+
+#include <opencv2\highgui\highgui.hpp>
+
+int main()
+{
+	cvNamedWindow("Streaming", CV_WINDOW_AUTOSIZE);
+	CvCapture* capture = cvCreateCameraCapture(0);
+	IplImage* frame;
+	while (1)
+	{
+		frame = cvQueryFrame(capture);
+		if (!frame) break;
+		cvShowImage("Streaming", frame);
+		char c = cvWaitKey(33);
+		if (c == 27) break; // press escape to terminate the loop
+	}
+
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Streaming");
+	return 0;
+}
